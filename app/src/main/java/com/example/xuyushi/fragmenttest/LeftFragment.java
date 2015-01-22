38a1f0f173ba13d733a9ae1b9e64183af17747a1
许雨石 <xuyushi@xuyushideMacBook-Pro.local>
2015-01-22 17:48:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xuyushi/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.xuyushi.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xuyushi on 15/1/21.
+ */
+public class LeftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        return view;
+
+    }
+}
---
+++ app/src/main/java/com/example/xuyushi/fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.example.xuyushi.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xuyushi on 15/1/21.
+ */
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        return view;
+    }
+}
